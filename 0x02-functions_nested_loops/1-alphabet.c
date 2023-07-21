adfd7941869c736de027e33c8b90f75b60869da6
SelasiEwurama <ewuramaselasi@gmail.com>
2023-07-20 21:00:31 -0700

    task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * print alphabet - function to print alphabets in lower case
- * always 0
+ * print_alphabet - function to print alphabet in lower case 
+ * Return: 0
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char i;
-
-	for (i = 'a' ; i <= 'z' i++)
+	for (i = 'a' ; i <= 'z' ; i++)
 		_putchar(i);
 	_putchar('\n');
 }
